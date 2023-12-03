fb2256e8e3c006e482dfdf5459ea1f3cfbdb4479
master <alya26271@gmail.com>
2023-12-03 07:44:00 +0200

    ArrayList, LinkedList, Queue

--- src/InterfaceMyHashMap.java
+++ src/InterfaceMyHashMap.java
@@ -1,7 +1,7 @@
 public interface InterfaceMyHashMap<K, V> {
-    public void put(K key, V value); //додає пару ключ + значення
-    public void remove(K key); //видаляє пару за ключем
-    public void clear(); //очищає колекцію
-    public int size(); //повертає розмір колекції
-    public V get(K key); //повертає значення (Object value) за ключем
+    void put(K key, V value); //додає пару ключ + значення
+    void remove(K key); //видаляє пару за ключем
+    void clear(); //очищає колекцію
+    int size(); //повертає розмір колекції
+    V get(K key); //повертає значення (Object value) за ключем
 }

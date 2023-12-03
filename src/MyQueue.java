c860d8f4ef2bc71d4af726d362631beeb825bef9
master <alya26271@gmail.com>
2023-12-03 03:35:39 +0200

    ArrayList, LinkedList

---
+++ src/MyQueue.java
@@ -0,0 +1,27 @@
+public class MyQueue<T> implements InterfaceMyQueue<T>{
+
+    @Override
+    public void add(T value) {
+
+    }
+
+    @Override
+    public void clear() {
+
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    @Override
+    public T peek() {
+        return null;
+    }
+
+    @Override
+    public T poll() {
+        return null;
+    }
+}
